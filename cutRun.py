56ead0ecb5c14f9896d0d72756f35816f5edcc5d
jcowman <joe.r.cowman@gmail.com>
2015-05-14 19:52:25 -0500

    Sprite import and demo

--- cutRun.py
+++ cutRun.py
@@ -7,7 +7,8 @@ from pygame.locals import *
 from sys import exit
 
 SCREENX, SCREENY = (1080,720)
-GAMEX,GAMEY = (1080,720)
+GAMEX,GAMEY = (320,208)
+GRIDSIZE = 16
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -23,6 +24,51 @@ DISPLAYSURF = pygame.display.set_mode((SCREENX,SCREENY))
 global gameSurf
 gameSurf = pygame.Surface((GAMEX,GAMEY))
 
+def get_spritesheet(filename):
+    sheet = pygame.image.load(filename).convert_alpha()
+    return sheet
+
+def get_sprites(surf,step):
+    w,h = surf.get_size()
+    numX,numY = (w/step,h/step)
+    x,y = (0,0)
+    surfList = []
+
+    for t in xrange(numX*numY):
+        s = pygame.Surface((step,step),0,32)
+        s.fill((1,1,1))
+        s.set_colorkey((1,1,1))
+        surfList.append(s)
+
+    for r in xrange(numY):
+        y= r*step
+        
+        for c in xrange(numX):
+            x = c*step
+            surfList[c+r*numX].blit(surf,(0,0),(c*step,r*step,step,step))
+
+    return surfList
+
+def demo_sprites(spriteList,destSurf,step):
+    w,h = destSurf.get_size()
+    numX = w/step
+    x,y = (0,0)
+    
+    for s in spriteList:
+        if x/step > numX:
+            x = 0
+            y += step
+
+        destSurf.blit(s,(x,y))
+
+        x += step
+
+    return destSurf
+        
+
+sheet1 = get_spritesheet("example1.png")
+list1 = get_sprites(sheet1,GRIDSIZE)
+
 while True:
 
     for event in pygame.event.get():
@@ -30,6 +76,10 @@ while True:
             exit()
 
     gameSurf.fill(GRAY1)
+
+    gameSurf = demo_sprites(list1,gameSurf,GRIDSIZE)
+    #gameSurf.blit(list1[1],(0,0))
+    #gameSurf.blit(sheet1,(0,0),(16, 80, 16, 16))
 
     if (SCREENX,SCREENY) != (GAMEX,GAMEY):
         pygame.transform.scale(gameSurf,(SCREENX,SCREENY),DISPLAYSURF)
